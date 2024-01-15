d06e27fa46cc40b9193b49caef2556f9a5198172
rdwayne876 <rdwayne876@gmail.com>
2024-01-15 02:26:23 +0100

    initial commit

---
+++ lib/auth/signup.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/logo.png"
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
