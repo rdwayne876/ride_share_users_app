8c8d6392c51ece1c4e15a9f02c11ae72ed8fd525
rdwayne876 <rdwayne876@gmail.com>
2024-03-04 21:48:18 -0500

    Firebase user registration

--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:users_app/auth/login.dart';
 import 'package:users_app/methods/common.dart';
+import 'package:users_app/pages/home.dart';
+import 'package:users_app/widgets/loading_dialog.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -34,7 +39,70 @@ class _SignUpState extends State<SignUp> {
       cMethods.displaySnackBar("Password must be at least 8 characters ", context);
     } else {
       //Register user
+      registerNewUser();
     }
+  }
+
+  registerNewUser() async{
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) => LoadingDialog(messageText: "Registering your account...")
+    );
+
+  //   final User? userFirebase;
+  //   try {
+  //     final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+  //         email: emailTextEditingController.text.trim(),
+  //         password: passwordTextEditingController.text.trim());
+  //     userFirebase = credential.user;
+  //   } on FirebaseAuthException catch (error) {
+  //     Navigator.pop(context)
+  //     cMethods.displaySnackBar(error.message!.toString(), context);
+  //   }
+  //
+  //   Navigator.pop(context);
+  //
+  //   DatabaseReference usersRef = FirebaseDatabase.instance.ref().child('users').child(userFirebase!.uid);
+    final User? userFirebase;
+    
+    try{
+      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailTextEditingController.text.trim(), 
+          password: passwordTextEditingController.text.trim());
+      
+      userFirebase = credential.user;
+      
+      Navigator.pop(context);
+      
+      if( userFirebase!= null) {
+        final usersRef =  FirebaseDatabase.instance.ref().child('users').child(userFirebase.uid);
+
+        Map userDataMap = {
+          "name": userNameTextEditingController.text.trim(),
+          "email": emailTextEditingController.text.trim(),
+          "phone": phoneTextEditingController.text.trim(),
+          "id": userFirebase.uid,
+          "blockedStatus": "no"
+        };
+
+        usersRef.set(userDataMap);
+        
+        Navigator.push(context, MaterialPageRoute(builder: (c) => HomePage()));
+      } else{
+        if (kDebugMode) {
+          print("User creation failed");
+        }
+      }
+    } on FirebaseAuthException catch ( error) {
+      Navigator.pop(context);
+      cMethods.displaySnackBar(error.message!.toString(), context);
+    } catch( error) {
+      if( kDebugMode){
+        print(error. toString());
+      }
+    }
+    
   }
 
   @override
